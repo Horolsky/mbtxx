b18b6826c50109f4da148b7ddddb3aa38885c6ee
Oleksandr Khorolskyi <oleksandr.khorolskyi@outlook.com>
2024-04-26 22:48:20 +0300

    add mbtxx lua package loader

--- mbtxx_cpp/demo/scripts/test.lua
+++ mbtxx_cpp/demo/scripts/test.lua
@@ -1,10 +1,8 @@
 local mymodule = require("mymodule")
 mymodule.hello_lua()
 
-
 local args = {...}
 print("Script args: " .. table.concat(args, ", "))
 
-
 local mbtxx = require("mbtxx")
-mbtxx.hello()
+mbtxx.hello_boost()
--- mbtxx_cpp/mbtxx/app_lua_handler.hpp
+++ mbtxx_cpp/mbtxx/app_lua_handler.hpp
@@ -19,6 +19,8 @@
 #include "mbtxx/external.hpp"
 #include "mbtxx/app_cli_handler.hpp"
 
+#include "hello_world.hpp"
+
 namespace mbtxx::app {
 
 class LuaHandler
@@ -27,12 +29,28 @@ class LuaHandler
     std::string const script_path_;
     sol::load_result script_;
 
+    void load_mbtxx_package()
+    {
+        sol::function require = lua_["require"];
+        auto new_require = [this, require](std::string const& mod_name) -> sol::table {
+            if (mod_name == "mbtxx") {
+                auto package = lua_.create_table();
+                package.set_function("hello_boost", hello_boost);
+                return package;
+            }
+            return require(mod_name);
+        };
+
+        lua_.set_function("require", new_require);
+    }
+
   public:
     LuaHandler(std::string const& script_path, std::string lib_path)
         : script_path_(script_path)
     {
-        lua_.open_libraries();
 
+        lua_.open_libraries();
+        load_mbtxx_package();
         std::string modules_path = std::filesystem::path(script_path_)
             .parent_path()
             .append("?.lua")
